05b1e627cafc1afe37b9cbecd939852c524979d9
DanielOhn <ohndaniel@gmail.com>
2022-09-21 16:19:41 -0700

    Add basic placeholder content i wanna include on the website.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,32 @@ function App() {
   return (
     <div className="App">
       <div className="header">
-        This is the header
+        <h1>Daniel Ohn</h1>
       </div>
       <div className='content'>
-        This is the content page...
+        <p>Software Developer, currently looking for work</p>
+        <p>Link to my resume</p>
+      </div>
+      <div className='social'>
+        Twitter, Github, Twitch
+      </div>
+      <div className='projects'>
+        <h2>Projects</h2>
+        <div>
+          brisketbot - a discord bot made with discordjs
+        </div>
+        <div>
+          possum pals - imageboard made with a pern stack
+        </div>
+        <div>
+          Poké App - react website that pulls from pokemon api
+        </div>
+        <div>
+          Pong - made pong with pixijs 
+        </div>
+        <div>
+          kayne tweets - react website that pulls api for kanye tweets
+        </div>
       </div>
     </div>
   )
